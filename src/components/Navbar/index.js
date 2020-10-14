f2fc76155fb70a0e56e1cce1e16f17862b9fe967
laurenho025 <laurenh4@illinois.edu>
2020-10-14 14:17:39 -0500

    Initial splash redesign for new theme

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 
-import Logo from "../../assets/eoh-general-logo-white.png";
+import Logo from "../../assets/eoh-puzzle-logo.png";
+import menuIcon from "../../assets/icons/menu.png";
 
 class Navbar extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            menuOpen: false,
+        }
+    }
+
+    toggleMenu = () => {
+        this.setState(prevState => ({
+            menuOpen: !prevState.menuOpen,
+        }));
+    }
+
+    hideMenu = () => {
+        this.setState(prevState => ({
+            menuOpen: false,
+        }));
+    }
+
     render() {
         return (
             <nav className="navbar">
@@ -11,10 +32,12 @@ class Navbar extends Component {
                     <img className="navbar-logo" src={Logo} alt="logo" />
                 </Link>
 
-                <div className="navbar-links">
-                    <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSd76ndm0137Ud-vfG9h-8tTbobY-v0n_X_3VBaDkaVxXthm9g/viewform?usp=sf_link">Exhibitors</a>
-                    <Link className="navbar-text" to="/sponsors">Sponsor Us</Link>
-                    {/* <Link className="navbar-text" to="/about">About Us</Link> */}
+                <img className="menu-icon" src={menuIcon} onClick={this.toggleMenu} />
+
+                <div className={`navbar-links ${this.state.menuOpen ? 'menu-open': 'menu-close'}`}>
+                    <a className="navbar-text" onClick={this.hideMenu} target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSd76ndm0137Ud-vfG9h-8tTbobY-v0n_X_3VBaDkaVxXthm9g/viewform?usp=sf_link">Exhibitors</a>
+                    <Link className="navbar-text" onClick={this.hideMenu} to="/sponsors">Sponsor Us</Link>
+                    {/* <Link className="navbar-text" onClick={this.hideMenu} to="/about">About Us</Link> */}
                 </div>
             </nav>
         );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,52 @@
 import React, { Component } from "react";
+import ReactTextRotator from "react-text-rotator";
+
+const words = [
+    {
+        text: "Overcome",
+        className: "rotating-text",
+        animation: "fade",
+    },
+    {
+        text: "Discover",
+        className: "rotating-text",
+        animation: "fade",
+    },
+    {
+        text: "Succeed",
+        className: "rotating-text",
+        animation: "fade",
+    },
+    {
+        text: "Triumph",
+        className: "rotating-text",
+        animation: "fade",
+    },
+    {
+        text: "Achieve",
+        className: "rotating-text",
+        animation: "fade",
+    },
+    {
+        text: "Continue",
+        className: "rotating-text",
+        animation: "fade",
+    },
+];
 
 class Home extends Component {
     render() {
         return (
             <div className="home">
-                <div className="event-name">
-                    <h1>Engineering Open House 2021</h1>
-                    <h3>University of Illinois at Urbana-Champaign</h3>
+                <div className="event-theme">
+                    <h1>TOGETHER WE WILL</h1>
+                    <ReactTextRotator
+                        content={words}
+                        time={4000}
+                    />
                 </div>
-                <div className="splash-message">
-                    <h4>To the members of our EOH Community:</h4>
-                    <p> 
-                        At this point in time, the Engineering Open House Central Committee is taking 
-                        the state of our campus and country into consideration as we proceed to make decisions 
-                        for EOH 2021. We do not take this global pandemic lightly. Therefore, to prioritize the safety and 
-                        well-being of our students, visitors, and the Champaign-Urbana community, we will be 
-                        creating an EOH like never before.
-                    </p>
-                    <div />
-                    <p>
-                        Our hope is to create a virtual presence that will 
-                        allow for a more accessible event, and that will continue to hold the 
-                        same spirit of EOH from the past 100 years: to have the students of the University of 
-                        Illinois showcase the world of engineering in the hopes of 
-                        inspiring our larger community.
-                    </p>
-                    <div />
-                    <p>We appreciate your patience and support at this time, and we plan to update our website 
-                        and social media platforms with our final decision soon.
-                    </p>
-                    <p id="message-signature">— Emily Roth, Director of Engineering Open House 2021</p>
+                <div className="event-details">
+                    <h4>March 26 - 27, 2021</h4>
                 </div>
             </div>
         );
