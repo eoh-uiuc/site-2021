622ba40053b94497890d26f6c7fd8a76abf968f3
Jack-Lanser <56211464+Jack-Lanser@users.noreply.github.com>
2020-10-18 17:24:50 -0500

    Added FAQ (#1)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Sponsor from "./pages/Sponsor";
+import FAQ from "./pages/FAQ";
 import Error from "./pages/Error";
 // import About from "./pages/About";
 
@@ -16,6 +17,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/sponsors" exact component={Sponsor} />
+          <Route path="/faq" exact component={FAQ} />
           {/* <Route path="/about" exact component={About} /> */}
           <Route exact component={Error} />
         </Switch>
