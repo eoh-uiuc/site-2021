de56d5785818826f023041594834f247fd83d619
laurenho025 <laurenh4@illinois.edu>
2021-03-22 06:17:10 -0500

    Work on tag styling

--- src/components/Pins/index.js
+++ src/components/Pins/index.js
@@ -2,12 +2,25 @@ import React from "react";
 import PinIcon from "../../assets/pin.png";
 import { Marker } from "react-map-gl";
 
+const PIN_SIZE = 35;
+
 const Pins = ({ data, handlePinClick }) => {
-	return data.map(
-		building => <Marker key={building.name} latitude={building.latitude} longitude={building.longitude}>
-			<img className="exhibits-pin-image" src={PinIcon} height={30} onClick={() => handlePinClick(building)} alt="pin" />
-		</Marker>
-	);
-}
+  return data.map((building) => (
+    <Marker
+      key={building.name}
+      latitude={building.latitude}
+      longitude={building.longitude}
+    >
+      <img
+        className="exhibits-pin-image"
+        style={{ transform: `translate(${-PIN_SIZE / 2}px,${-PIN_SIZE}px)` }}
+        src={PinIcon}
+        height={PIN_SIZE}
+        onClick={() => handlePinClick(building)}
+        alt="pin"
+      />
+    </Marker>
+  ));
+};
 
 export default Pins;
--- src/pages/Exhibits/exhibitData.js
+++ src/pages/Exhibits/exhibitData.js
@@ -1,313 +1,332 @@
 const exhibitData = [
-	{
-		name: 'Mechanical Engineering Laboratory',
-		link: "/exhibits/mechanical-engineering-laboratory",
-		latitude: 40.1121,
-		longitude: -88.2263,
-		description: "In Spring 2019, construction began on a three-story addition to the south face of Talbot Laboratory.",
-		tags: "mechanical",
-		exhibits: [
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-		],
-	},
-	{
-		name: 'Everitt Laboratory',
-		link: "/exhibits/everitt-laboratory",
-		latitude: 40.1111,
-		longitude: -88.22841,
-		description: "In Spring 2019, construction began on a three-story addition to the south face of Talbot Laboratory.",
-		exhibits: [
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				tags: ["mechanical",
-						"easy",
-						"quick",
-						"natural"
-					],
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				tags: ["mechanical",
-						"easy",
-					],
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				tags: ["mechanical",
-						"difficult",
-						"long"
-					],
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				tags: ["mechanical",
-						"easy",
-						"long"
-					],
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-		],	
-	},
-	{
-		name: 'Talbot Laboratory',
-		link: "/exhibits/talbot-laboratory",
-		latitude: 40.11215,
-		longitude: -88.22844,
-		description: "In Spring 2019, construction began on a three-story addition to the south face of Talbot Laboratory.",
-		exhibits: [
-			{
-				name: "Exhibit Name Name Name Name Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-		],
-	},
-	{
-		name: 'Materials Science and Engineering',
-		link: "/exhibits/materials-science",
-		latitude: 40.1111,
-		longitude: -88.2263,
-		description: "In Spring 2019, construction began on a three-story addition to the south face of Talbot Laboratory.",
-		exhibits: [
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-			{
-				name: "Exhibit Name",
-				rso: "Engineering Council",
-				video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
-				description: "2-3 sentences describing the exhibit video.",
-				links: [
-					{
-						name: "RSO Site",
-						address: "http://www.ec.illinois.edu",
-					},
-					{
-						name: "Read More",
-						address: "http://www.google.com"
-					},
-				],
-			},
-		],
-	},
+  {
+    name: "Nuclear Engineering Laboratory",
+    link: "/exhibits/nuclear-engineering-laboratory",
+    latitude: 40.111239,
+    longitude: -88.224408,
+    description:
+      "In Spring 2019, construction began on a three-story addition to the south face of Talbot Laboratory. The expansion enhances the educational experience of NPRE students by giving them access to state-of-the-art laboratories for radiation measurements and nuclear materials.",
+    exhibits: [
+      {
+        name: "Women in Nuclear",
+        rso: "Women in Nuclear",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description:
+          "Learn about nuclear science with the UIUC Women in Nuclear chapter! We talk about radiation, nuclear power plants, and plasmas.",
+        tags: ["Kid-Friendly", "Molecular Scale", "Physics"],
+        links: [],
+      },
+      {
+        name: "American Nuclear Society",
+        rso: "American Nuclear Society",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "Interactive projects exploring everything nuclear!",
+        tags: ["Environment", "Sustainable", "Physics"],
+        links: [],
+      },
+    ],
+  },
+  {
+    name: "Mechanical Engineering Laboratory",
+    link: "/exhibits/mechanical-engineering-laboratory",
+    latitude: 40.1121,
+    longitude: -88.2263,
+    description:
+      "In Spring 2019, construction began on a three-story addition to the south face of Talbot Laboratory.",
+    tags: "",
+    exhibits: [
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Everitt Laboratory",
+    link: "/exhibits/everitt-laboratory",
+    latitude: 40.1111,
+    longitude: -88.22841,
+    description:
+      "In Spring 2019, construction began on a three-story addition to the south face of Talbot Laboratory.",
+    exhibits: [
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        tags: ["mechanical", "easy", "quick", "natural"],
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        tags: ["mechanical", "easy"],
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        tags: ["mechanical", "difficult", "long"],
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        tags: ["mechanical", "easy", "long"],
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Talbot Laboratory",
+    link: "/exhibits/talbot-laboratory",
+    latitude: 40.11215,
+    longitude: -88.22844,
+    description:
+      "In Spring 2019, construction began on a three-story addition to the south face of Talbot Laboratory.",
+    exhibits: [
+      {
+        name: "Exhibit Name Name Name Name Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Materials Science and Engineering",
+    link: "/exhibits/materials-science",
+    latitude: 40.1111,
+    longitude: -88.2263,
+    description:
+      "In Spring 2019, construction began on a three-story addition to the south face of Talbot Laboratory.",
+    exhibits: [
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+      {
+        name: "Exhibit Name",
+        rso: "Engineering Council",
+        video: "https://www.youtube.com/watch?v=fLM5F0u0D9E",
+        description: "2-3 sentences describing the exhibit video.",
+        links: [
+          {
+            name: "RSO Site",
+            address: "http://www.ec.illinois.edu",
+          },
+          {
+            name: "Read More",
+            address: "http://www.google.com",
+          },
+        ],
+      },
+    ],
+  },
 ];
-  
+
 export default exhibitData;
