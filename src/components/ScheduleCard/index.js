783371e4a68d3a9059b5e19389da1721b6e0ec90
Jack-Lanser <jlanser2@illinois.edu>
2021-03-21 23:33:35 -0500

    finished up the live schedule with info

--- src/components/ScheduleCard/index.js
+++ src/components/ScheduleCard/index.js
@@ -6,7 +6,6 @@ const ScheduleCard = ({ data }) => {
 			<p className="schedule-card-time">{`${data.startTime} - ${data.endTime}`}</p>
 			<h4 className="schedule-card-name">{data.name}</h4>
 			<p className="schedule-card-description">{data.description}</p>
-			<a target="_blank" rel="noopener noreferrer" href={data.zoom}><button className="red-link-button schedule-card-link">Join Zoom</button></a>
 			{data.links.map(l => <a target="_blank" rel="noopener noreferrer" href={l.link}><button className="red-link-button schedule-card-link">{l.name}</button></a>)}
 		</div>
 	);
