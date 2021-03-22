9a0cbf84ab00b16605f6beee7a4c41496f975598
laurenho025 <laurenh4@illinois.edu>
2021-03-22 03:08:49 -0500

    Remove sponsor payment page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import Sponsor from "./pages/Sponsor";
 import Error from "./pages/Error";
 import Visitors from "./pages/Visitors";
 import About from "./pages/About";
@@ -17,7 +16,6 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/sponsors" exact component={Sponsor} />
           <Route path="/visitors" exact component={Visitors} />
           <Route path="/about" exact component={About} />
           <Route path="/schedule" exact component={Schedule} />
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,70 +1,95 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import Logo from "../../assets/eoh-puzzle-logo.png";
 import { Squash as Hamburger } from "hamburger-react";
 
 class Navbar extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            menuOpen: false,
-            top: true,
-        }
+    this.state = {
+      menuOpen: false,
+      top: true,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    if (window.scrollY === 0) {
+      this.setState({ top: true });
+    } else {
+      this.setState({ top: false });
     }
+  };
 
-    componentDidMount() {
-        window.addEventListener("scroll", this.handleScroll);
-    }
+  toggleMenu = () => {
+    console.log("here");
+    this.setState((prevState) => ({
+      menuOpen: !prevState.menuOpen,
+    }));
+  };
 
-    componentWillUnmount() {
-        window.removeEventListener("scroll", this.handleScroll);
-    }
+  hideMenu = () => {
+    this.setState((prevState) => ({
+      menuOpen: false,
+    }));
+    window.scrollTo(0, 0);
+  };
 
-    handleScroll = () => {
-        if (window.scrollY === 0) {
-            this.setState({ top: true });
-        } else {
-            this.setState({ top: false });
-        }
-    }
+  render() {
+    return (
+      <nav
+        className={`navbar ${
+          this.state.menuOpen ? "navbar-open" : "navbar-close"
+        } ${!this.state.menuOpen && !this.state.top ? "scrolled" : ""}`}
+      >
+        <Link onClick={this.hideMenu} to="/">
+          <img className="navbar-logo" src={Logo} alt="logo" />
+        </Link>
 
-    toggleMenu = () => {
-        console.log("here");
-        this.setState(prevState => ({
-            menuOpen: !prevState.menuOpen,
-        }));
-    }
+        <div className="menu-icon">
+          <Hamburger
+            animation="butterfly"
+            toggle={this.toggleMenu}
+            toggled={this.state.menuOpen}
+          />
+        </div>
 
-    hideMenu = () => {
-        this.setState(prevState => ({
-            menuOpen: false,
-        }));
-        window.scrollTo(0, 0);
-    }
-
-    render() {
-        return (
-            <nav className={`navbar ${this.state.menuOpen ? 'navbar-open': 'navbar-close'} ${!this.state.menuOpen && !this.state.top ? 'scrolled': ''}`}>
-                <Link onClick={this.hideMenu} to="/">
-                    <img className="navbar-logo" src={Logo} alt="logo" />
-                </Link>
-
-                <div className="menu-icon">
-                    <Hamburger animation="butterfly" toggle={this.toggleMenu} toggled={this.state.menuOpen}/>
-                </div>
-
-                <div className={`navbar-links ${this.state.menuOpen ? 'menu-links-open': 'menu-links-close'}`}>
-                    <a className="navbar-text" onClick={this.hideMenu} target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSd76ndm0137Ud-vfG9h-8tTbobY-v0n_X_3VBaDkaVxXthm9g/viewform?usp=sf_link">Exhibitors</a>
-                    <Link className="navbar-text" onClick={this.hideMenu} to="/schedule">Schedule</Link>
-                    <Link className="navbar-text" onClick={this.hideMenu} to="/visitors">Visitors</Link>
-                    <Link className="navbar-text" onClick={this.hideMenu} to="/sponsors">Sponsor Us</Link>
-                    <Link className="navbar-text" onClick={this.hideMenu} to="/about">About Us</Link>
-                </div>
-            </nav>
-        );
-    }
+        <div
+          className={`navbar-links ${
+            this.state.menuOpen ? "menu-links-open" : "menu-links-close"
+          }`}
+        >
+          <a
+            className="navbar-text"
+            onClick={this.hideMenu}
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSd76ndm0137Ud-vfG9h-8tTbobY-v0n_X_3VBaDkaVxXthm9g/viewform?usp=sf_link"
+          >
+            Exhibitors
+          </a>
+          <Link className="navbar-text" onClick={this.hideMenu} to="/schedule">
+            Schedule
+          </Link>
+          <Link className="navbar-text" onClick={this.hideMenu} to="/visitors">
+            Visitors
+          </Link>
+          <Link className="navbar-text" onClick={this.hideMenu} to="/about">
+            About Us
+          </Link>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
