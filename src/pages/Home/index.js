cf177649f6235f4ad24e2b5d47c795481fc19ce4
laurenho025 <laurenh4@illinois.edu>
2021-03-24 03:22:55 -0500

    Display sponsor logos on home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,56 +1,58 @@
 import React, { Component } from "react";
 import ReactTextRotator from "react-text-rotator";
 
+import Sponsors from "./sponsors";
+
 const words = [
-    {
-        text: "Overcome",
-        className: "rotating-text",
-        animation: "fade",
-    },
-    {
-        text: "Discover",
-        className: "rotating-text",
-        animation: "fade",
-    },
-    {
-        text: "Succeed",
-        className: "rotating-text",
-        animation: "fade",
-    },
-    {
-        text: "Triumph",
-        className: "rotating-text",
-        animation: "fade",
-    },
-    {
-        text: "Achieve",
-        className: "rotating-text",
-        animation: "fade",
-    },
-    {
-        text: "Continue",
-        className: "rotating-text",
-        animation: "fade",
-    },
+  {
+    text: "Overcome",
+    className: "rotating-text",
+    animation: "fade",
+  },
+  {
+    text: "Discover",
+    className: "rotating-text",
+    animation: "fade",
+  },
+  {
+    text: "Succeed",
+    className: "rotating-text",
+    animation: "fade",
+  },
+  {
+    text: "Triumph",
+    className: "rotating-text",
+    animation: "fade",
+  },
+  {
+    text: "Achieve",
+    className: "rotating-text",
+    animation: "fade",
+  },
+  {
+    text: "Continue",
+    className: "rotating-text",
+    animation: "fade",
+  },
 ];
 
 class Home extends Component {
-    render() {
-        return (
-            <div className="home">
-                <div className="event-theme">
-                    <h1>TOGETHER WE WILL</h1>
-                    <ReactTextRotator
-                        content={words}
-                        time={4000}
-                    />
-                </div>
-                <div className="event-details">
-                    <h4>March 26 - 27, 2021</h4>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="home-splash">
+          <div className="event-theme">
+            <h1>TOGETHER WE WILL</h1>
+            <ReactTextRotator content={words} time={4000} />
+          </div>
+          <div className="event-details">
+            <h4>March 26 - 27, 2021</h4>
+          </div>
+        </div>
+        <Sponsors />
+      </div>
+    );
+  }
 }
 
 export default Home;
---
+++ src/pages/Home/sponsors.js
@@ -0,0 +1,115 @@
+import React from "react";
+
+import Ameren from "../../assets/sponsor-logos/ameren.jpeg";
+import Cat from "../../assets/sponsor-logos/cat.png";
+import Chevron from "../../assets/sponsor-logos/chevron.jpg";
+import Cummins from "../../assets/sponsor-logos/cummins.jpg";
+import Exelon from "../../assets/sponsor-logos/exelon.png";
+import Exxon from "../../assets/sponsor-logos/exxon.png";
+import Ford from "../../assets/sponsor-logos/ford.png";
+import JohnDeere from "../../assets/sponsor-logos/john-deere.png";
+import PG from "../../assets/sponsor-logos/pg.png";
+import RR from "../../assets/sponsor-logos/rolls-royce.jpg";
+import Synchrony from "../../assets/sponsor-logos/synchrony.jpg";
+
+const Sponsors = () => (
+  <div className="home-sponsor-logos">
+    <h2>Our Sponsors</h2>
+    <p>
+      Engineering Open House 2021 is proudly sponsored by the following
+      companies. Click on a logo to view more information about our sponsors!
+    </p>
+    <div className="sponsors-list">
+      <a
+        className="sponsors-logo"
+        href="https://www.harting.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={Ameren} alt="a" />
+      </a>
+      <a
+        className="sponsors-logo"
+        href="https://www.sbbrg.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={Cat} alt="ej" />
+      </a>
+      <a
+        className="sponsors-logo"
+        href="https://www.goldmansachs.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={Chevron} alt="g" />
+      </a>
+      <a
+        className="sponsors-logo"
+        href="https://www.synchrony.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={Cummins} alt="s" />
+      </a>
+      <a
+        className="sponsors-logo"
+        href="https://www.synchrony.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={Exelon} alt="s" />
+      </a>
+      <a
+        className="sponsors-logo"
+        href="https://www.synchrony.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={Exxon} alt="s" />
+      </a>
+      <a
+        className="sponsors-logo"
+        href="https://www.synchrony.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={Ford} alt="s" />
+      </a>
+      <a
+        className="sponsors-logo"
+        href="https://www.synchrony.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={JohnDeere} alt="s" />
+      </a>
+      <a
+        className="sponsors-logo"
+        href="https://www.synchrony.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={PG} alt="s" />
+      </a>
+      <a
+        className="sponsors-logo"
+        href="https://www.synchrony.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={RR} alt="s" />
+      </a>
+      <a
+        className="sponsors-logo"
+        href="https://www.synchrony.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={Synchrony} alt="s" />
+      </a>
+    </div>
+  </div>
+);
+
+export default Sponsors;
