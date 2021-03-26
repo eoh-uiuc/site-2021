3dfe199cd6935a24e224dc51be301de595ae2dd8
Jack-Lanser <jlanser2@illinois.edu>
2021-03-26 17:19:07 -0500

    fix asce video

--- src/pages/Exhibits/exhibitData.js
+++ src/pages/Exhibits/exhibitData.js
@@ -583,7 +583,7 @@ const exhibitData = [
       {
         name: "ASCE: Beginner Lessons in Civil Engineering!",
         rso: "American Society of Civil Engineers",
-        video: "https://youtu.be/yU3Gbdgvvig",
+        video: "https://youtu.be/wQuhuCOtGiE",
         description:
           "Come visit our virtual booth for intro lessons on some of our most popular civil engineering studies, including structural engineering, geotechnical engineering, and construction management! Email the team lead Rober Wiggins at rjw5@illinois.edu if you have any questions.",
         tags: ["Kid-Friendly", "Physics", "Construction"],
